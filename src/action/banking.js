24decf7c64b1d9f32201d402541b7cfc3b4b7326
Kuldeep Harshwal <kuldeep.harshwal@allonblock.com>
2018-08-27 13:14:14 +0530

    Promisable action done

--- src/action/banking.js
+++ src/action/banking.js
@@ -6,29 +6,41 @@ export const requestBankingData = subreddit => ({
     subreddit,
   });
 
-  export const receiveBankingData = (subreddit, json) => ({
+  export const receiveBankingData = (subreddit, json, id, resolve) => {
+    resolve(json);
+  return({
     type: BANKING_CONSTANTS.RECEIVED_BANKING_DATA,
     subreddit,
     data: json,
     receivedAt: Date.now(),
   });
+};
   
- export  const receiveBankingDataError = (subreddit, err, errCode) => ({
+ export  const receiveBankingDataError = (subreddit, err, errCode, reject) => {
+  reject(err);
+   return({
     type: BANKING_CONSTANTS.RECEIVED_BANKING_DATA_ERROR,
     subreddit,
     error: err,
     errorCode: errCode,
-  });
+  })
+};
 
-export const postBankingData = (data, subreddit, assignOrgUrl) => dispatch =>
-  dispatch(dynamicActionWrapper({
-    path: assignOrgUrl,
-    method: 'Post',
-    body: data,
-    initCb: requestBankingData,
-    successCb: receiveBankingData,
-    failureCb: receiveBankingDataError,
-    subreddit,
-    wrapperActionType: 'POST_ASSING_ORGANIZATIONS_TO_ITEMS_WRAPPER',
-  }));
+export const postBankingData = (data, subreddit, assignOrgUrl) => dispatch => {
+  return new Promise((resolve, reject) => {
+    dispatch(dynamicActionWrapper({
+      path: assignOrgUrl,
+      method: 'Post',
+      body: data,
+      initCb: requestBankingData,
+      successCb: receiveBankingData,
+      failureCb: receiveBankingDataError,
+      resolve: resolve,
+      reject: reject,
+      subreddit,
+      wrapperActionType: 'POST_ASSING_ORGANIZATIONS_TO_ITEMS_WRAPPER',
+    }));
+}) 
+}
+  
 
