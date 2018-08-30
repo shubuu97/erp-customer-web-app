b63f4ff5aa54a76153af97651e8c9b968e11dd2b
Mayank Jain <mayank.jain@allonblock.com>
2018-08-30 06:53:28 +0000

    Revert "added em tags (pull request #78)"
    This reverts pull request #78.
    
    > added em tags

--- src/components/CustomerProfile/AccountInfo/accountInfoFields.js
+++ src/components/CustomerProfile/AccountInfo/accountInfoFields.js
@@ -2,8 +2,8 @@ import { SelectFieldInput,TextFieldInput} from '../../../components/common/Mater
 
 
 let accountInfo=[
-    { name: 'contactNumber', label: 'Contact Number <em>*</em>', component: TextFieldInput ,type:'text'},
-    { name: 'licenseNumber', label: 'License Number <em>*</em>', component: TextFieldInput ,type:'text'},
+    { name: 'contactNumber', label: 'Contact Number *', component: TextFieldInput ,type:'text'},
+    { name: 'licenseNumber', label: 'License Number *', component: TextFieldInput ,type:'text'},
 ]
 
 export default accountInfo;
