639401efd337df254e478f9cda2fb078d74d6cea
Kuldeep Harshwal <kuldeep.harshwal@allonblock.com>
2018-09-24 14:04:02 +0530

    Multiple time acceptjs loaded bug resolves

--- src/containers/Products/payment/payWithCard.js
+++ src/containers/Products/payment/payWithCard.js
@@ -2,37 +2,13 @@ import React, { Component } from 'react';
 import orIcon from './../../../assets/images/or-icon.png';
 import axios from 'axios';
 import _find from 'lodash/find';
-import { findIndex, remove, indexOf } from 'lodash';
-function new_script(src) {
-  return new Promise(function (resolve, reject) {
-    var script = document.createElement('script');
-    script.src = src;
-    script.addEventListener('load', function () {
-      resolve();
-    });
-    script.addEventListener('error', function (e) {
-      reject(e);
-    });
-    document.body.appendChild(script);
-  })
-};
-
-const resAuth = function (data) {
-  console.log("Auth called");
-  // window.response(data);
-}
-// window.resAuth =resAuth;
 
 export default class PayWithCard extends Component {
   constructor(props) {
     super(props)
-    // window.response = this.response;
-    this.state = {
-      count: 0
-    }
 
     window.response = (data) => {
-      this.state.count = this.state.count + 1
+      console.log("Authorize.net load called");
       if (data.messages.resultCode === "Error") {
         var i = 0;
         while (i < data.messages.message.length) {
@@ -43,46 +19,37 @@ export default class PayWithCard extends Component {
           i = i + 1;
         }
       } else {
-        // if (this.state.count % 2 == 0)
         this.props.onPay(data);
       }
     }
 
   }
-  removeJS(filename){
+  removeJS(filename) {
     var tags = document.getElementsByTagName('script');
-    for (var i = tags.length; i >= 0; i--){ //search backwards within nodelist for matching elements to remove
-     if (tags[i] && tags[i].getAttribute('src') != null && tags[i].getAttribute('src').includes(filename))
-      tags[i].parentNode.removeChild(tags[i]); //remove element by calling parentNode.removeChild()
+    for (var i = tags.length; i >= 0; i--) { //search backwards within nodelist for matching elements to remove
+      if (tags[i] && tags[i].getAttribute('src') != null && tags[i].getAttribute('src').includes(filename))
+        tags[i].parentNode.removeChild(tags[i]); //remove element by calling parentNode.removeChild()
     }
-   }
+  }
+  new_script(src) {
+    return new Promise(function (resolve, reject) {
+      console.log("sccpp");
+      var script = document.createElement('script');
+      script.src = src;
+      script.addEventListener('load', function () {
+        console.log('loaded_kuldeep');
+        resolve();
+      });
+      script.addEventListener('error', function (e) {
+        reject(e);
+      });
+    })
+  };
 
   componentDidMount() {
-    // let acceptUiV3Found = false;
-    // let acceptUiV1Found = false;
-    // let alreadyExistIds = [];
-    // for (let i = 0; i < document.scripts.length; i++) {
-    //   if (document.scripts[i].src.includes('jstest.authorize.net')) {
-    //     alreadyExistIds.push(document.scripts[i]);
-    //   }
-    // }
-    // remove(document.scripts, function (script, key) {
-    //   return indexOf(alreadyExistIds, script) !== -1
-    // });
-    // this.removeJS('jstest.authorize.net');
-    // console.log(alreadyExistIds, "jj")
-    new_script("https://jstest.authorize.net/v3/AcceptUI.js");
-    new_script("https://jstest.authorize.net/v1/Accept.js").then((data) => {
-      console.log("chal gai", data);
-    })
-      .catch((err) => {
-        console.log(err)
-      })
-
+    this.removeJS('jstest.authorize.net');
+    this.new_script("https://jstest.authorize.net/v3/AcceptUI.js");
   }
-
-
-
 
   render() {
     return (
@@ -97,7 +64,7 @@ export default class PayWithCard extends Component {
           data-clientKey={this.props.detail.clientKey}
           data-acceptUIFormBtnTxt="Submit"
           data-acceptUIFormHeaderTxt="Card Information"
-          data-responseHandler="resAuth">Pay Using Card
+          data-responseHandler="response">Pay Using Card
 </button>
       </form>
     )
